13f3104a891400d8e1fd010561f98aabea7240c2
Hyogeun Kim <ultrasound2k@gmail.com>
2017-02-21 00:26:05 +0900

    readOBD using rawSQL

--- koolproject/readOBD/readOBD.py
+++ koolproject/readOBD/readOBD.py
@@ -1,4 +1,5 @@
-from readOBD.models import OBDdb
+#from readOBD.models import OBDdb
+from django.db import connection
 
 import obd
 
@@ -16,67 +17,64 @@ elif connection.status() == OBDStatus.ELM_CONNECTED:
 elif connection.status() == OBDStatus.CAR_CONNECTED:
     print("Car is connected")
 """
+def obddb_custom_sql(dataList):
+    sql = """
+            INSERT INTO readOBD_obddb (speed,
+                                       intake_pressure,
+                                       intake_temp,
+                                       rpm,
+                                       maf,
+                                       coolant_temp,
+                                       fuel_level,
+                                       ambiant_air_temp,
+                                       oil_temp,
+                                       control_module_voltage)
+                                VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+          """
+    with connection.cursor() as cursor:
+        cursor.execute(sql, (dataList[0], dataList[1], dataList[2], dataList[3], dataList[4], dataList[5], dataList[6], dataList[7], dataList[8], dataList[9]))
 
 # Check if a response is empty
 while 1:
-    #dataList = {}
-
+    dataList = []
     speed = ELMconnection.query(obd.commands.SPEED)
     if not speed.is_null():
-        speed = OBDdb.objects.create(speed=speed.value.magnitude)
-        speed.save()
-        #dataList["SPEED"] = str(speed.value)
+        dataList.append(speed.value.magnitude)
 
     intake_pressure = ELMconnection.query(obd.commands.INTAKE_PRESSURE)
     if not intake_pressure.is_null():
-        intake_pressure = OBDdb.objects.create(intake_pressure=intake_pressure.value.magnitude)
-        intake_pressure.save()
-        #dataList["INTAKE_PRESSURE"] = str(intake_pressure.value)
+        dataList.append(intake_pressure.value.magnitude)
 
     intake_temp = ELMconnection.query(obd.commands.INTAKE_TEMP)
     if not intake_temp.is_null():
-        intake_temp = OBDdb.objects.create(intake_temp=intake_temp.value.magnitude)
-        intake_temp.save()
-        #dataList["INTAKE_TEMP"] = str(intake_temp.value)
+        dataList.append(intake_temp.value.magnitude)
 
     rpm = ELMconnection.query(obd.commands.RPM)
     if not rpm.is_null():
-        rpm = OBDdb.objects.create(rpm=rpm.value.magnitude)
-        rpm.save()
-        #dataList["RPM"] = str(rpm.value)
+        dataList.append(rpm.value.magnitude)
 
     maf = ELMconnection.query(obd.commands.MAF)
     if not maf.is_null():
-        maf = OBDdb.objects.create(maf=maf.value.magnitude)
-        maf.save()
-        #dataList["MAF"] = str(maf.value)
+        dataList.append(maf.value.magnitude)
 
     coolant_temp = ELMconnection.query(obd.commands.COOLANT_TEMP)
     if not coolant_temp.is_null():
-        coolant_temp = OBDdb.objects.create(coolant_temp=coolant_temp.value.magnitude)
-        coolant_temp.save()
-        #dataList["COOLANT_TEMP"] = str(coolant_temp.value)
+       dataList.append(coolant_temp.value.magnitude)
 
     fuel_level = ELMconnection.query(obd.commands.FUEL_LEVEL)
     if not fuel_level.is_null():
-        fuel_level = OBDdb.objects.create(fuel_level=coolant_temp.value.magnitude)
-        fuel_level.save()
-        #dataList["FUEL_LEVEL"] = str(fuel_level.value)
+       dataList.append(coolant_temp.value.magnitude)
 
     ambiant_air_temp = ELMconnection.query(obd.commands.AMBIANT_AIR_TEMP)
     if not ambiant_air_temp.is_null():
-        ambiant_air_temp = OBDdb.objects.create(ambiant_air_temp=ambiant_air_temp.value.magnitude)
-        ambiant_air_temp.save()
-        #dataList["AMBIANT_AIR_TEMP"] = str(ambiant_air_temp.value)
+        dataList.append(ambiant_air_temp.value.magnitude)
 
     oil_temp = ELMconnection.query(obd.commands.OIL_TEMP)
     if not oil_temp.is_null():
-        oil_temp = OBDdb.objects.create(oil_temp=oil_temp.value.magnitude)
-        oil_temp.save()
-        #dataList["OIL_TEMP"] = str(oil_temp.value)
+        dataList.append(oil_temp.value.magnitude)
 
     control_module_voltage = ELMconnection.query(obd.commands.CONTROL_MODULE_VOLTAGE)
     if not control_module_voltage.is_null():
-        control_module_voltage = OBDdb.objects.create(control_module_voltage=control_module_voltage.value.magnitude)
-        control_module_voltage.save()
-        #dataList["CONTROL_MODULE_VOLTAGE"] = str(control_module_voltage.value)
+        dataList.append(control_module_voltage.value.magnitude)
+
+    obddb_custom_sql(dataList)
